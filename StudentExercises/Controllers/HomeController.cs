6457975ba174c18a38d59dd1ce9449dd60efc6b9
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-13 19:13:15 -0500

    Moved db calls to controller

--- StudentExercises/Controllers/HomeController.cs
+++ StudentExercises/Controllers/HomeController.cs
@@ -29,9 +29,15 @@ namespace StudentExercises.Controllers
             }
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var viewModel = new StudentInstructorViewModel(_config.GetConnectionString("DefaultConnection"));
+            var viewModel = new StudentInstructorViewModel();
+            List<Task> tasks = new List<Task>()
+            {
+                Task.Run(async () => viewModel.Students = await GetAllStudents()),
+                Task.Run(async () => viewModel.Instructors = await GetAllInstructors()),
+            };
+            await Task.WhenAll(tasks);
             return View(viewModel);
         }
 
@@ -44,6 +50,78 @@ namespace StudentExercises.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        private async Task<List<Student>> GetAllStudents()
+        {
+            var students = new List<Student>();
+            using (SqlConnection conn = Connection)
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            Id,
+                            FirstName,
+                            LastName,
+                            SlackHandle
+                        FROM Student";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    students = new List<Student>();
+                    while (await reader.ReadAsync())
+                    {
+                        students.Add(new Student
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                        });
+                    }
+
+                    reader.Close();
+                }
+            }
+            return students;
+        }
+
+        private async Task<List<Instructor>> GetAllInstructors()
+        {
+            var instructors = new List<Instructor>();
+            using (SqlConnection conn = Connection)
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT
+                            Id,
+                            FirstName,
+                            LastName,
+                            SlackHandle
+                        FROM Instructor";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    instructors = new List<Instructor>();
+                    while (await reader.ReadAsync())
+                    {
+                        instructors.Add(new Instructor
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                        });
+                    }
+
+                    reader.Close();
+                }
+            }
+            return instructors;
         }
     }
 }
